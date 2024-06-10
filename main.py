527f2176192570181b9efdaaa3393056547d83fc
alexey007o <olegolieg63@gmail.com>
2024-06-10 23:52:35 +0300

    some part, comments, special symbols

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Lesson 3
 #Link: https://www.youtube.com/watch?v=ML5tP8m6SHw&list=PLDyJYA6aTY1lPWXBPk0gw6gR8fEtPDGKa&index=3
-print('Hello itProger')
+print('Результат: ', 5, 154, ".", sep='|', end='\n')
+print('_ another text')
 #разработка комментариев
