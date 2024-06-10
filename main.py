1ec3d3a20f531cbfb2ab42eee12c84b7d011fb0c
alexey007o <olegolieg63@gmail.com>
2024-06-11 00:05:27 +0300

    end lesson

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #Lesson 3
 #Link: https://www.youtube.com/watch?v=ML5tP8m6SHw&list=PLDyJYA6aTY1lPWXBPk0gw6gR8fEtPDGKa&index=3
 print('Результат: ', 5, 154, ".", sep='|', end='\n')
-print('_ another text')
+print('_ another text', round(5.2 / 2))
 #разработка комментариев
+text = input("Введите ваш возраст: ")
+print(text + ". Столько ему лет.")
